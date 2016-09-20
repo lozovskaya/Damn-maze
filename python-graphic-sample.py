fac0855810d0813e2ac95fa3271b2a24eae24395
derrior <s16b2_aleykin@179.ru>
2016-09-20 18:27:53 +0300

    keybindings, more comments

--- python-graphic-sample.py
+++ python-graphic-sample.py
@@ -3,7 +3,9 @@ import tkinter
 Rad = 10
 
 def idle_function():
-    command = input()
+    # write here your code, which must evaluate every tick
+    command = ""
+#    command = input() # uncomment it to move the circle, but after that char_func won't work
     if (command == 'left'):
         current_coords[0] -= 10
     elif (command == 'right'):
@@ -14,7 +16,13 @@ def idle_function():
         current_coords[1] += 10
     drawingStuff.coords(ball, current_coords[0], current_coords[1],
                             current_coords[0] + Rad, current_coords[1] + Rad)
+                            # creation of new object is too long. I think, you should
+                            # keep all your objects, and move it. Remove - move to the point (-179, -179)
     root.after(20, idle_function)
+
+
+def char_func(arg):
+    print("You have pressed that", str(arg.keysym), "with code:", arg.keysym_num)
 
 root = tkinter.Tk()
 drawingStuff = tkinter.Canvas(root)
@@ -26,4 +34,6 @@ print("type in your cmd commands 'left', 'right', 'up' and 'down' to move the ci
 
 drawingStuff.pack()
 idle_function()
+root.bind("<Key>", char_func)
+#  see help("tkinter.Tk.bind") for more callbacks
 root.mainloop()
