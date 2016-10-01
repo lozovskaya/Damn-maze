45840cade4ee7190ca0ebdbd9a54169a70e68c63
derrior <s16b2_aleykin@179.ru>
2016-10-01 21:39:36 +0300

    beginning of server

--- basic_graphics.py
+++ basic_graphics.py
@@ -1,25 +1,36 @@
-from tkinter import * 
+import tkinter as tk
 
 def close(event):
     root.destroy()
     root.quit()
 
-def player(x1, y1, x2, y2):
+
+def create_player(x1, y1, x2, y2):
     player = canvas.create_oval(x1, y1, x2, y2, fill="blue")
     
 
-def create_one_wall(x1, y1, x2, y2):
-    wall = canvas.create_rectangle(x1, y1, x2, y2, fill="brown", outline="black", width=4)
+# def create_one_wall(x1, y1, x2, y2):
+  #  wall = canvas.create_rectangle(x1, y1, x2, y2, fill="brown", outline="black", width=4)
     
     
-def maze(x1, y1, x2, y2):
+def create_square(x1, y1, x2, y2, array):
     maze = canvas.create_rectangle(x1, y1, x2, y2, outline="black", width=5)
+    array.append(maze)
+    return array
 
-    
-root = Tk() 
-canvas = Canvas(root, width=300, height=300)
+
+def create_button(text_on_button, x1, y1):
+    button = tk.Button(root, width=15, height=3, bg="white", fg="black", text=text_on_button, font="arial 15")
+    button.place(x=x1, y=y1)
+
+
+def hide_me(event):
+    widget.pack_forget()
+
+
+root = tk.Tk()
+array_of_squares = []
+canvas = tk.Canvas(root, width=300, height=300)
 canvas.pack()
-button = Button(root, text="Exit", width=15, height=3, bg="white", fg="black") 
-button.place(x=100, y=120)
-button.bind("<Button-1>", close)
+create_button("Exit", 0, 0)
 root.mainloop()
---
+++ server.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main():
+{
+    int n=5;
+    int field[n][n];
+    for (i = 0; i < n; ++i)
+    {
+        for (j = 0; j < n; ++i)
+            field[i][j] = 0
+    }
+    return 0;
+}
