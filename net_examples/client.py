5b434c9dce7ccb85186ef5285e980937e976010b
derrior <s16b2_aleykin@179.ru>
2016-10-02 16:53:52 +0300

    other way to send data in net_examples/server.cpp

--- net_examples/client.py
+++ net_examples/client.py
@@ -6,10 +6,12 @@ sock = socket.socket()
 sock.connect((ip, Port))
 #передача строки
 sock.send(bytearray("Hello, world".encode("utf-8")))
-print(sock.recv(255))
+data = sock.recv(255)
+print(data, len(data))
 #передача числа
 print("type a number")
 n = int(input())
+ba = n.to_bytes(4, byteorder)
 sock.send(n.to_bytes(4, byteorder)) 
 #(что передать).to_bytes(сколько байт получить, byteorder)
 print(int.from_bytes(sock.recv(255), byteorder))
--- net_examples/server.cpp
+++ net_examples/server.cpp
@@ -2,7 +2,8 @@
    The port number is passed as an argument */
 #include <stdio.h> //printf
 #include <string.h> //memcpy
-#include <unistd.h> //read write functions
+#include <stdlib.h>
+//#include <unistd.h> //read write functions
 #include <netinet/in.h> // all socket functions
 #ifndef _WIN32
     typedef int SOCKET;
@@ -46,27 +47,27 @@ int main()
           return 0;
      }
      bzero(buffer, 256);
-     int n = read(client_socket, buffer, 255); // getting data from client_socket, writing it in buffer, max_amount of data is 255 bytes
+     int n = recv(client_socket, buffer, 255, MSG_CONFIRM); // getting data from client_socket, writing it in buffer, max_amount of data is 255 bytes
      if (n < 0) // n - amount of bytes you just have got. if it is negative, something is bad
      {
          printf("ERROR reading from socket\n");
          return 0;
      }
-     printf("Here is the message: %s\n", buffer);
-     n = write(client_socket, "I got your message", 18); // write to the first parameter, from second parameter, amount of bytes - third parameter
-     n = read(client_socket, buffer, 255);
+     printf("Here is the message: %d %s\n", n, buffer);
+     n = send(client_socket, "I got your message", 18, MSG_CONFIRM); // write to the first parameter, from second parameter, amount of bytes - third parameter
+     n = recv(client_socket, buffer, 255, MSG_CONFIRM);
      int number;
      memcpy(&number, buffer, 4); // Скопировать (третий аргумент) байтов из (второй аргумент) в (первый аргумент)
      printf("%d\n", number);
      number++;
      memcpy(buffer, &number, 4);
-     write(client_socket, buffer, 4);  
+     n = send(client_socket, buffer, 4, MSG_CONFIRM);  
      if (n < 0)
      {
         printf("ERROR writing to socket\n");
         return 0;
      }
-     close(client_socket);
-     close(my_socket);
+     shutdown(client_socket, SHUT_RDWR);
+     shutdown(my_socket, SHUT_RDWR);
      return 0; 
 }
