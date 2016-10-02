b696ccaa79fdd0a3bedcf820ef2ef62b2a70085e
derrior <s16b2_aleykin@179.ru>
2016-10-02 14:22:25 +0300

    unnecessary includes deleted

--- net_examples/server.cpp
+++ net_examples/server.cpp
@@ -1,12 +1,9 @@
 /* A simple server in the internet domain using TCP
    The port number is passed as an argument */
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h> 
-#include <sys/socket.h>
-#include <netinet/in.h>
+#include <stdio.h> //printf
+#include <string.h> //memcpy
+#include <unistd.h> //read write functions
+#include <netinet/in.h> // all socket functions
 #ifndef _WIN32
     typedef int SOCKET;
 #endif
