cccf713e05d1ced8fa88476d52f17f4df9229ddb
derrior <s16b2_aleykin@179.ru>
2016-10-03 17:18:17 +0300

    added script to look at maze

---
+++ minimap.py
@@ -0,0 +1,24 @@
+import png
+
+
+def get_color(x):
+    if x == 0:
+        return (200, 200, 200, 255)
+
+    return (64, 32, 0, 255)
+
+def get_pixel(i, j):
+    if (i % width == 0 or j % width == 0):
+        return (0, 0, 0, 255)
+    else:
+        return isWall[i // width][j // width]
+
+f = open("field.txt")
+n, m = map(int, f.readline().rstrip().split())
+width = 1000 // (n + m)
+isWall = [0 for i in range(n)]
+for i in range(n):
+    isWall[i] = list(map(get_color, map(int, f.readline().rstrip().split())))
+new_array = [[get_pixel(i, j) for j in range(m * width)] for i in range(n * width)]
+
+png.from_array(new_array, "RGBA").save("field.png")
