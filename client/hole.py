cdab8104e05df9e688e605b91bbfc22253deed9e
derrior <s16b2_aleykin@179.ru>
2016-10-28 16:25:59 +0300

    more pretty

--- client/hole.py
+++ client/hole.py
@@ -1,22 +1,23 @@
 import tkinter as tk
+
+
 def to_hex_digit(i):
     if (i < 10):
         return str(i)
     return chr(ord('a') + i - 10)
 
+
 def to_hex(i):
-    curr_pow = 10
+    curr_pow = 16 ** 10
     answer = []
-    print(i)
     while (curr_pow >= 0):
-        if (i >= 16 ** curr_pow):
-            div, remainer = divmod(i, (16 ** curr_pow))
+        if (i >= curr_pow):
+            div, remainer = divmod(i, curr_pow)
             answer.append(to_hex_digit(div))
             i = remainer
         elif (len(answer) > 0):
             answer.append('0')
-        curr_pow -= 1
-    print(answer)
+        curr_pow /= 16
     return ''.join(answer).rjust(2, '0')
 
 root = tk.Tk()
@@ -25,6 +26,6 @@ canvas.pack()
 for i in range(5, -1, -1):
     hole = canvas.create_rectangle(110 + i * 2, 110 + i * 2, 
                                    100 - i * 2, 100 - i * 2, outline="black",
-                                   fill="#" + str(to_hex(i * 40)) * 6)
+                                   fill="#" + to_hex(i * 40) * 6)
 # print(hole)
 root.mainloop()
