1a7e7a0d4275fb363bafe355fd0311037178e8b9
lozovskaya <alinalozov445@gmail.com>
2016-11-10 15:43:56 +0300

    added something

--- client/basic_graphics.py
+++ client/basic_graphics.py
@@ -2,6 +2,7 @@
 
 import tkinter as tk
 import take_from_server
+import hole
 width_of_square = 50
 
 def close():
@@ -13,17 +14,9 @@ def create_player(x1, y1, x2, y2):
     player = canvas.create_oval(x1, y1, x2, y2, fill="blue")
     return player
 
-'''
-def create_one_wall(x1, y1, x2, y2):
-    wall = canvas.create_rectangle(x1, y1, x2, y2, fill="brown", outline="black", width=4)   
-def add_square(maze, array):
-    # maze = canvas.create_rectangle(x1, y1, x2, y2, outline="black", width=5)
-    array.append(maze)
-    return array'''
 
-
-def create_one_square(x1, y1, x2, y2, colour="white"):
-    square = canvas.create_rectangle(x1, y1, x2, y2, outline="black", fill=colour)
+# def create_one_square(x1, y1, x2, y2, colour="white"):
+#1    square = canvas.create_rectangle(x1, y1, x2, y2, outline="black", fill=colour)
 
 
 def create_button(text_on_button, x1, y1, command1):
@@ -36,25 +29,31 @@ def hide_me(event):
     widget.pack_forget()
 
     
-def add_new_square(array_of_squares, i, j, colour="white"):
+def add_new_square(i, j, colour="white"):
     # clouds = ["black", "white", "grey", "ref", "green", "blue", "cyan", "magenta", "yellow"]
-    new_square = create_one_square(j * width_of_square + 10, i * width_of_square + 10,
+    new_square = canvas.create_rectangle(j * width_of_square + 10, i * width_of_square + 10,
                                    j * width_of_square + width_of_square + 10, 
-                                   i * width_of_square + width_of_square + 10, colour)
-    array_of_squares.append(new_square)
-    return array_of_squares
+                                   i * width_of_square + width_of_square + 10, 
+                                   outline="black", fill=colour)
+    return new_square
+
+def f():
+    for i in range(len(field)):                                                      
+        for j in range(len(field[i])):                                               
+            if field[i][j] == 0:                                                     
+                array_of_squares += add_new_square(i, j)                            
+            elif field[i][j] == 1:                                                   
+                array_of_squares += add_new_square(i, j, "brown")                    
+            #elif field[i][j] == 2:                                                   
+                
+    root.after(f, 10)
 
 
 root = tk.Tk()
-array_of_squares = []
 canvas = tk.Canvas(root, height=1000, width=1500)
 canvas.pack()
 sock = take_from_server.init()
 field = take_from_server.return_array(sock)
-for i in range(len(field)):
-    for j in range(len(field[i])):
-        if field[i][j] == 0:
-            array_of_squares = add_new_square(array_of_squares, i, j)
-        elif field[i][j] == 1:
-            array_of_squares = add_new_square(array_of_squares, i, j, "brown")
+f()
+#hole.create_hole(canvas)
 root.mainloop()
--- client/hole.py
+++ client/hole.py
@@ -10,22 +10,25 @@ def to_hex_digit(i):
 def to_hex(i):
     curr_pow = 16 ** 10
     answer = []
-    while (curr_pow >= 0):
+    while (curr_pow >= 1):
         if (i >= curr_pow):
             div, remainer = divmod(i, curr_pow)
             answer.append(to_hex_digit(div))
             i = remainer
         elif (len(answer) > 0):
             answer.append('0')
-        curr_pow /= 16
+        curr_pow //= 16
     return ''.join(answer).rjust(2, '0')
 
+
+def create_hole(canvas, x, y):
+    for i in range(5, -1, -1):
+        hole = canvas.create_rectangle(x + i * 2, y + i * 2, 
+                                       x - 50 - i * 2, y - 50 - i * 2, outline="black",
+                                       fill="#" + to_hex(i * 40) * 6)
+
 root = tk.Tk()
-canvas = tk.Canvas(root, height=300, width=300)
+canvas = tk.Canvas(root)
 canvas.pack()
-for i in range(5, -1, -1):
-    hole = canvas.create_rectangle(110 + i * 2, 110 + i * 2, 
-                                   100 - i * 2, 100 - i * 2, outline="black",
-                                   fill="#" + to_hex(i * 40) * 6)
-# print(hole)
+create_hole(canvas, 100, 100)
 root.mainloop()
