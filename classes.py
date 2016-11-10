4cee5475a320bb0902f0ffba926d7adf5e0359b1
lozovskaya <alinalozov445@gmail.com>
2016-11-10 15:29:17 +0300

    added stop_draw

--- classes.py
+++ classes.py
@@ -1,15 +1,19 @@
 import tkinter as tk
 width_of_square = 50
+root = tk.Tk()
 
 
-class draw:
-    # def __init__(self):
+
+class drawing:
+    array_of_squares = []
+    canvas = tk.Canvas(root)
+    canvas.pack()
     def start_draw(self):
-        self.k = 0
-    def draw(self, canvas, array_of_squares, x, y, colour):
-        if self.k < len(array_of_squares):
-            canvas.coords(array_of_squares[self.k], x, y, x + width_of_square, y + width_of_square)
-            self.k += 1
+        self.index = 0
+    def draw(self, x, y, colour, canvas=canvas, array_of_squares=array_of_squares):
+        if self.index < len(array_of_squares):
+            canvas.coords(array_of_squares[self.index], x, y, x + width_of_square, y + width_of_square)
+            self.index += 1
         else:
             new_square = canvas.create_rectangle(x * width_of_square + 10,
                                              y * width_of_square + 10, 
@@ -17,17 +21,16 @@ class draw:
                                              y * width_of_square + width_of_square + 10, 
                                              outline="black", fill=colour)
             array_of_squares.append(new_square)
-            self.k += 1
+            self.index += 1
+    def stop_draw(self, canvas=canvas, array_of_squares=array_of_squares):
+        for i in range(self.index, len(array_of_squares)):
+            canvas.coords(array_of_squares[i], -1, -1)
 
 
-root = tk.Tk()
-canvas = tk.Canvas(root)
-canvas.pack()
-array_of_squares = []
-drawit = draw()
-drawit.start_draw()
-drawit.draw(canvas, array_of_squares, 1, 2, "black")
-drawit.start_draw()
-drawit.draw(canvas, array_of_squares, 2, 3, "red")
-print(drawit.k)
+d = drawing()
+d.start_draw()
+d.draw(1, 1, "black")
+d.draw(2, 2, "red")
+d.start_draw()
+d.draw(3, 3, "blue")
 root.mainloop()
